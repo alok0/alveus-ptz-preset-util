8dc44851b687136e1c22904d11305826e60e5cb2
alok <alok0@users.noreply.github.com>
2025-05-23 12:45:24 -0400

    switch to hash routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { App } from "./App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { useHashLocation } from "wouter/use-hash-location";
+import { Router } from "wouter";
 
 createRoot(document.body).render(
   <React.StrictMode>
-    <App />
+    <Router hook={useHashLocation}>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
