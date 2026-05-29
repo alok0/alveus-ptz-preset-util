01e5cdea7b019e1691215c1f4bf3d19e2fa40e71
alok <alok0@users.noreply.github.com>
2026-05-28 21:25:05 -0400

    hide emu cam

--- src/cams.ts
+++ src/cams.ts
@@ -29,6 +29,13 @@ export const cams = [
 ] as const;
 export type CamType = (typeof cams)[number];
 
-const HiddenCams = new Set(["noodle", "roach", "marty", "patchy", "event"]);
+const HiddenCams = new Set([
+  "noodle",
+  "roach",
+  "marty",
+  "patchy",
+  "event",
+  "emu",
+]);
 
 export const isCamHidden = (v: string) => HiddenCams.has(v);
