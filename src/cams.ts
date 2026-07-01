11b8dcc25446ce3d62530b05299a1d28a67006e9
alok <alok0@users.noreply.github.com>
2026-07-01 15:08:55 -0400

    hide pushin page

--- src/cams.ts
+++ src/cams.ts
@@ -31,6 +31,13 @@ export const cams = [
 ] as const;
 export type CamType = (typeof cams)[number];
 
-const HiddenCams = new Set(["noodle", "roach", "marty", "patchy", "event"]);
+const HiddenCams = new Set([
+  "noodle",
+  "roach",
+  "marty",
+  "patchy",
+  "event",
+  "pushpopindoor",
+]);
 
 export const isCamHidden = (v: string) => HiddenCams.has(v);
