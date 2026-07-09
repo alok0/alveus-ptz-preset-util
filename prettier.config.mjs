57da49a4a5affae9cba5076a8087d579e3e0f202
alok <alok0@users.noreply.github.com>
2026-07-09 09:55:38 -0400

    change formatting on database.json for searchability/readability

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
   trailingComma: "all",
+  overrides: [
+    {
+      files: "./src/database.json",
+      options: { printWidth: 200, objectWrap: "collapse" },
+    },
+  ],
 };
 
 export default config;
